34a1dd01cda65c04ba2faf71ef8baa5d390141d9
Lucasej800 <luca.ah.fly@icloud.com>
2021-11-09 17:13:40 +0100

    Sending SpaceInvaders2

--- Space-Invaders-2/SpaceInvaders2.py
+++ Space-Invaders-2/SpaceInvaders2.py
@@ -2,8 +2,11 @@ import pygame
 pygame.init()
 
 
-w, h = 700, 600
+w, h = 690, 600
 screen = pygame.display.set_mode((w, h))
+pygame.display.set_caption("Space Invaders")
+logo = pygame.image.load("logo.png")
+pygame.display.set_icon(logo)
 
 FPS = 25
 SHOOT_COOLDOWN = 0
@@ -51,22 +54,100 @@ class Player(pygame.sprite.Sprite):
         if self.bullet_rect.y <= 0:
             self.shoot = False
 
-        if self.rect.x:
-            self.rect.move(0, self.rect.y)
-
-    def collision(self):
-        pass
 
 class Enemy(pygame.sprite.Sprite):
-    pass
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.location_y = 0
+        self.image = pygame.image.load("enemy3.png")
+        self.rect = self.image.get_rect()
+        self.enemy_line1 = [
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+        ]
+        self.enemy_line2 = [
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+            self.rect,
+        ]
+
+        self.time = 0
+        self.speed = 5
+        self.amount = len(self.enemy_line1) + len(self.enemy_line2)
+        self.living = []
+
+        self.xposition = 0
+        self.yposition = 0
+        self.xpos = []
+        self.ypos = []
+
+        for i in range(self.amount):
+            self.xpos.append(self.xposition)
+            self.xposition += 50
+            self.ypos.append(self.yposition)
+            self.living.append(True)
+
+
+    def draw(self):
+        for i in range(self.amount // 2):
+            self.enemy_line1[i].x = self.xpos[i]
+            self.enemy_line1[i].y = self.ypos[i]
+            if self.enemy_line1[i].colliderect(player.bullet_rect):
+                if self.living[i]:
+                    player.shoot = False
+                    print("dead")
+
+            if not self.enemy_line1[i].colliderect(player.bullet_rect):
+                if self.living[i]:
+                    screen.blit(self.image, self.enemy_line1[i])
+            else:
+                self.living[i] = False
+
+    def move(self):
+        if self.time == 25 or self.time == 50 or self.time == 75 or self.time == 100:
+            for i in range(len(self.xpos)):
+                self.xpos[i] += 25
+        if self.time == 125:
+            for i in range(self.amount):
+                self.ypos[i] += 50
+        if self.time == 150 or self.time == 175 or self.time == 200 or self.time == 225:
+            for i in range(len(self.xpos)):
+                self.xpos[i] -= 25
+        if self.time == 250:
+            for i in range(self.amount):
+                self.ypos[i] += 25
+        if self.time == 250:
+            self.time = 0
+            self.speed += 1
+
 
 player = Player()
 enemy = Enemy()
 
 clock = pygame.time.Clock()
 
-while True: # Main GameLoop
+while True:  # Main GameLoop
     # EventLoop
+    enemy.time += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             pygame.quit()
@@ -76,5 +157,7 @@ while True: # Main GameLoop
     screen.fill((0, 0, 0))
     player.draw()
     player.movement()
+    enemy.draw()
+    enemy.move()
     pygame.display.update()
     clock.tick(FPS)
